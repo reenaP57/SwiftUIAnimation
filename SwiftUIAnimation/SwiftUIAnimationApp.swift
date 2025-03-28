1b34935be18357b68f95550471c2e18845953209
Reena Prajapati <reenaben.prajapati@capgemini.com>
2025-03-28 19:49:32 +0530

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Reenaben Prajapati on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAnimationTests/SwiftUIAnimationTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationTests.swift
+//  SwiftUIAnimationTests
+//
+//  Created by Reenaben Prajapati on 28/03/25.
+//
+
+import Testing
+@testable import SwiftUIAnimation
+
+struct SwiftUIAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
